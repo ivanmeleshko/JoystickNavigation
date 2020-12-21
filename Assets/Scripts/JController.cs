ff29dd9d87c3ad0ef480f2c26b6edbcbba34301a
ivanmeleshko <ivan22meleshko@gmail.com>
2020-12-21 19:27:38 +0200

    first commit

---
+++ Assets/Scripts/JController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+public class JController : MonoBehaviour
+{
+
+    [SerializeField]
+    FixedJoystick MoveJoystick;
+    [SerializeField]
+    Joybutton JumpButton;
+    [SerializeField]
+    FixedTouchField TouchField;
+
+
+    void Update()
+    {
+        var fps = GetComponent<RigidbodyFirstPersonController>();
+
+        fps.RunAxis = MoveJoystick.Direction;
+        fps.JumpAxis = JumpButton.Pressed;
+        fps.mouseLook.LookAxis = TouchField.TouchDist;
+
+    }
+}
---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+
+public class MyScript : MonoBehaviour
+{
+
+    protected Joystick joystick;
+    protected Joybutton joybutton;
+    protected bool jump;
+
+
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<Joybutton>();
+    }
+
+
+    void Update()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+
+        rigidBody.velocity = new Vector3(joystick.Horizontal * 100f + Input.GetAxis("Horizontal") * 100f,
+                                         rigidBody.velocity.y,
+                                         joystick.Vertical * 100f+ Input.GetAxis("Vertical") * 100f);
+
+        if (!jump && (joybutton.Pressed || Input.GetButton("Fire2")))
+        {
+            jump = true;
+            rigidBody.velocity += Vector3.up * 20f;
+        }
+
+        if (jump && !joybutton.Pressed || Input.GetButton("Fire2"))
+        {
+            jump = false;
+        }
+
+    }
+}
